5ffb5c045e146e034c9709ee0a7fa1550037398c
Alobal123 <miroslavkrabec@seznam.cz>
2019-04-12 10:38:29 +0200

    Better look of tables, figures, added code references.

--- dockers/data_conversion/visualize_points.py
+++ dockers/data_conversion/visualize_points.py
@@ -6,17 +6,30 @@ import h5py
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("-f", default= "D:\\workspace\\diplomka\\src\\3Dconversion\\test_0_0.h5", type=str, help="npz file to load")
+    
+    with open("D:\\workspace\\diplomka\\data\\points\\ply_data_test_0_id2file.json", 'r') as f:
+        s = f.read()
+        s = s.split(',')
+        for i, word in enumerate(s):
+            if 'airplane_0627' in word:
+                print(i, word)
+        
+    path = "D:\\workspace\\diplomka\\data\\points\\ply_data_test0.h5"
+    #path = "D:\\workspace\\diplomka\\data\\test_0.h5"
+    
+    parser.add_argument("-f", default= path, type=str, help="npz file to load")
     args = parser.parse_args()
     
     f = h5py.File(args.f)
-    print(f)
-    index = 0
+    index = 143
     labels = np.array(f['label'])
     points = np.array(f['data'])
-    soms = np.array(f['som'])
+    print(points.shape)
+    #soms = np.array(f['som'])
     fig = plt.figure()
     ax = Axes3D(fig)
-    #ax.scatter(points[index,:,0], points[index,:,1],points[index,:,2])
-    ax.scatter(soms[index,:,0], soms[index,:,1],soms[index,:,2])
+    ax.axis("off")
+    ax.scatter(points[index,:,0], points[index,:,1],points[index,:,2])
+    #ax.scatter(soms[index,:,0], soms[index,:,1],soms[index,:,2])
     plt.show()
+    
--- dockers/data_conversion/voxel_vizualization.py
+++ dockers/data_conversion/voxel_vizualization.py
@@ -5,8 +5,12 @@ import matplotlib.pyplot as plt
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
+    
+    path = "D:\\workspace\\diplomka\\data\\modelnet40_rot_test.npz"
+    #path = "D:\\workspace\\diplomka\\data\\test.npz"
+    
     parser.add_argument("-v", default=32, type=int, help="Resolution of the voxel grid")
-    parser.add_argument("-f", default= "D:\\workspace\\diplomka\\src\\3Dconversion\\modelnet40_rot_test.npz", type=str, help="npz file to load")
+    parser.add_argument("-f", default= path, type=str, help="npz file to load")
     parser.add_argument("-r", default = 24, type=int, help="Number of rotations of model along vertical axis")
     args = parser.parse_args()
 
@@ -15,14 +19,17 @@ if __name__ == '__main__':
     yt = np.asarray(np.load(args.f)['targets'],dtype=np.float32)
     N = args.v
     index = 0
-    ma = xt[index].reshape((N,N,N))
+    view = 0
+        
+    ma = xt[index+view].reshape((N,N,N))
     print(np.count_nonzero(xt[0] - xt[1]))
     
     print(xt.shape)
     fig = plt.figure()
     ax = fig.gca(projection='3d')
+    ax.axis("off")
     ax.set_aspect('equal')
     
-    ax.voxels(ma)
+    ax.voxels(ma,edgecolors='b')
     
     plt.show()
